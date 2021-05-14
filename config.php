e76b1bb4d46c55bce715b52475f2f4ccb88ff0b0
jankat <jankat@sabanciuniv.edu>
2021-05-14 20:09:38 +0300

    add delete functions of all items working

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // throw exceptions
-$db = mysqli_connect('localhost','root','','notebook');
+$db = mysqli_connect('localhost','root','','notebook_app');
 if($db->connect_errno > 0){
 	die('Unable to connect database ['. $db->connect_error. ']');
 }
--- index.php
+++ index.php
@@ -100,7 +100,7 @@ button {
 }
  
 .label {
-  color: ;
+  color: #FB667A;
   padding: 8px;
   font-family: Arial;
 }
@@ -169,7 +169,7 @@ button {
 	<tbody>
 		<?php
 			include "config.php";
-			$sql_statement = "SELECT * FROM Users";
+			$sql_statement = "SELECT * FROM users";
 			$result = mysqli_query($db,$sql_statement);
 			while($row = mysqli_fetch_assoc($result)){
 				$id = $row['uid'];
@@ -177,31 +177,26 @@ button {
 				$pwd = $row['pwd'];
 				$name = $row['name'];
 				$email = $row['e_mail'];
-				echo "<tr>"."<td>".$id."<td>".$nick."<td>".$pwd."<td>".$name."<td>".$email."<td>"."<form action = '' method = 'POST'>".
-				"<input type = 'hidden' value=".$id." name = 'delete'>"."<button style = 'background-color: #ea061d;'> Delete </button>"."</form>" ."<tr>";
+				echo "<tr>"."<td>".$id."<td>".$nick."<td>".$pwd."<td>".$name."<td>".$email."<td>"."<form action = '' method = 'POST'>"."<input type = 'hidden' value=".$id." name = 'delete'>"."<button style = 'background-color: #ea061d;'> Delete </button>"."</form>" ."<tr>";
 			}
 			if(isset($_POST['delete'])){
 				$id = $_POST['delete'];
-
-				$sql_statement = "DELETE FROM Users WHERE Users.uid = $id";
+				$sql_statement = "DELETE FROM users WHERE Users.uid = $id";
 				$result = mysqli_query($db,$sql_statement);
 				if($result != 1){
 					echo mysqli_error();
 				}
 				else{
 					header("location: index.php");
+					exit();
 				}
-
 			}
-
 			if(isset($_POST['submit'])){
-				
 				$nickname = $_POST['nickname'];
 				$pwd = $_POST['pwd'];
 				$name = $_POST['name'];
 				$e_mail = $_POST['e_mail'];
-
-				$sql_statement = "INSERT INTO Users(uid, nickname, pwd, name, e_mail)
+				$sql_statement = "INSERT INTO users(uid, nickname, pwd, name, e_mail)
                         VALUES (DEFAULT,'$nickname','$pwd','$name', '$e_mail')";
 				$insert = mysqli_query($db,$sql_statement);
 				if($insert != 1){
@@ -209,6 +204,7 @@ button {
 				}
 				else{
 					header("location: index.php");
+					exit();
 				}
 			}
 			?>
@@ -248,16 +244,47 @@ button {
 			$price = $row['price'];
 			$cname = $row['course_name'];
 			echo "<tr>"."<td>".$id."<td>".$title."<td>".$desc."<td>".$price
-			."<td>".$cname."<td>"."<button style = 'background-color: #ea061d;' type='button', >Delete</button>"."<tr>";
+			."<td>".$cname."<td>"."<form action = '' method = 'POST'>".
+				"<input type = 'hidden' value=".$id." name = 'deletenote'>"."<button style = 'background-color: #ea061d;'> Delete </button>"."</form>" ."<tr>";
+		}
+		if(isset($_POST['deletenote'])){
+			$id = $_POST['deletenote'];
+			$sql_statement = "DELETE FROM note WHERE note.note_id = $id";
+			$result = mysqli_query($db,$sql_statement);
+			if($result != 1){
+				echo mysqli_error();
+			}
+			else{
+				header("location: index.php");
+				exit();
+			}
+		}
+		if(isset($_POST['notesubmit'])){
+			$title = $_POST['title'];
+			$desc = $_POST['desc'];
+			$price = $_POST['price'];
+			$cname = $_POST['course_name'];
+			$sql_statement = "INSERT INTO note(note_id,title,description,price, course_name)
+                VALUES (DEFAULT,'$title','$desc','$price', '$cname')";
+			$insert = mysqli_query($db,$sql_statement);
+			if($insert != 1){
+				echo mysqli_error();
+			}
+			else{
+				header("location: index.php");
+				exit();
+			}
 		}
 		?>
 		<tr>
+			<form method="POST">
 			<td><span class="label">Id</span></td>
 			<td><input type="text" name="title"></td>
 			<td><input type="text" name="desc"></td>
 			<td><input type="text" name="price"></td>
 			<td><input type="text" name="course_name"></td>
-			<td><button style = 'background-color: #1fee0c;'type="button ">Add</button></td>
+			<td><input style = 'background-color: #1fee0c;'type="submit" name="notesubmit" value="Add"></td>
+			</form>
 		</tr>
 	</tbody>
 </table>
@@ -277,17 +304,46 @@ button {
 		$sql_statement = "SELECT * FROM orders";	
 		$result = mysqli_query($db,$sql_statement);
 		while($row = mysqli_fetch_assoc($result)){
-			$oid = $row['oid'];
-			$price = $row['price'];
-			$date = $row['date'];
-			echo "<tr>"."<td>".$oid."<td>".$price."<td>".$date."<td>"."<button style = 'background-color: #ea061d;' type='button', >Delete</button>"."<tr>";			
+		$oid = $row['oid'];
+		$price = $row['price'];
+		$date = $row['date'];
+		echo "<tr>"."<td>".$oid."<td>".$price."<td>".$date."<td>"."<form action = '' method = 'POST'>".
+			"<input type = 'hidden' value=".$oid." name = 'deleteorder'>"."<button style = 'background-color: #ea061d;'> Delete </button>"."</form>" ."<tr>";		
+		}
+		if(isset($_POST['deleteorder'])){
+			$id = $_POST['deleteorder'];
+			$sql_statement = "DELETE FROM orders WHERE orders.oid = $id";
+			$result = mysqli_query($db,$sql_statement);
+			if($result != 1){
+				echo mysqli_error();
+			}
+			else{
+				header("location: index.php");
+				exit();
+			}
+		}	
+		if(isset($_POST['ordersubmit'])){
+			$price = $_POST['price'];
+			$date = $_POST['date'];
+			$sql_statement = "INSERT INTO orders(`oid`,price,`date`)
+                    VALUES (DEFAULT,'$price','$date')";
+			$insert = mysqli_query($db,$sql_statement);
+			if($insert != 1){
+				echo mysqli_error();
+			}
+			else{
+				header("location: index.php");
+				exit();
+			}
 		}
 		?>
 		<tr>
-			<td><input type="text" name="order_id"></td>
+			<form method="POST">
+			<td><span class="label">Id</span></td>
 			<td><input type="text" name="price"></td>
 			<td><input type="text" name="date"></td>
-			<td><button style = 'background-color: #1fee0c;'type="button ">Add</button></td>
+			<td><input style = 'background-color: #1fee0c;'type="submit" name="ordersubmit" value="Add"></td>
+			</form>
 		</tr>
 	</tbody>
 </table>
