fe585f7c7e8e249991bc94df6022d3effa64731b
Alperen KARAVELIOGLU <67004083+Alpovka@users.noreply.github.com>
2021-05-15 21:16:54 +0300

    added animated buttons with and jQuery fadeOut

--- mycart.php
+++ mycart.php
@@ -17,6 +17,13 @@
     />
     <link rel="stylesheet" href="mycart.css">
     <title>Notebook/My Cart</title>
+    <script>
+         $(document).ready(function() {
+            $(".trash").click(() => {
+                $(".item").fadeOut("slow");
+            });
+         });
+    </script>
 </head>
 <body>
     <div class="top">
@@ -38,17 +45,46 @@
                         </div>
                         <p>Lorem20Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias voluptate assumenda harum quam sapiente velit eaque veritatis incidunt officia consequuntur.</p>
                         <label for="posted">Posted By: </label>
-                        <button id="posted-name" name="posted" style="background: #DCDCDC;border: none;">Jhon Doe</button>
+                        <button id="posted-name" name="posted" style="background: none;border: none;">Jhon Doe</button>
                         <br>
                         <label for="other">Contact info: </label>
-                        <button id="mail" name="other" style="background: #DCDCDC;border: none;">example@email.com</button>
+                        <button id="mail" name="other" style="background: none;border: none;">example@email.com</button>
                     </form>
                     <div class="two-box">
-                    <svg xmlns="http://www.w3.org/2000/svg" style ="cursor: pointer;" width="24"       height="24" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                    </svg>
-                    <h6>5$</h6>
+                        <button style="background: none;border: none">
+                        <section style="padding: 0;">
+                            <span class="trash" style="transform: scale(0.3);cursor: pointer;">
+                                <span></span>
+                                <i></i>
+                            </span>
+                        </section>
+                        </button>
+                        <h6>5$</h6>
+                    </div>
+                </div>
+                <div class="item">
+                    <img src="https://images.pexels.com/photos/6749464/pexels-photo-6749464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="lecture note photo" id="item-img">
+                    <form class="content">
+                        <div class="card-top">
+                            <h6>best c300 notes!</h6><p id="lecture">CS300</p>
+                        </div>
+                        <p>Lorem20ncidunt officia consequuntur.</p>
+                        <label for="posted">Posted By: </label>
+                        <button id="posted-name" name="posted" style="background: none;border: none;">Jhon Doe</button>
+                        <br>
+                        <label for="other">Contact info: </label>
+                        <button id="mail" name="other" style="background: none;border: none;">example@email.com</button>
+                    </form>
+                    <div class="two-box">
+                        <button style="background: none;border: none">
+                        <section style="padding: 0;">
+                            <span class="trash" style="transform: scale(0.3);cursor: pointer;">
+                                <span></span>
+                                <i></i>
+                            </span>
+                        </section>
+                        </button>
+                        <h6>5$</h6>
                     </div>
                 </div>
                 <div class="item">
@@ -59,38 +95,21 @@
                         </div>
                         <p>Lorem20Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias voluptate assumenda harum quam sapiente velit eaque veritatis incidunt officia consequuntur.</p>
                         <label for="posted">Posted By: </label>
-                        <button id="posted-name" name="posted" style="background: #DCDCDC;border: none;">Jhon Doe</button>
+                        <button id="posted-name" name="posted" style="background: none;border: none;">Jhon Doe</button>
                         <br>
                         <label for="other">Contact info: </label>
-                        <button id="mail" name="other" style="background: #DCDCDC;border: none;">example@email.com</button>
+                        <button id="mail" name="other" style="background: none;border: none;">example@email.com</button>
                     </form>
                     <div class="two-box">
-                    <svg xmlns="http://www.w3.org/2000/svg" style ="cursor: pointer;" width="24"       height="24" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                    </svg>
-                    <h6>5$</h6>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="https://images.pexels.com/photos/6749464/pexels-photo-6749464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="lecture note photo" id="item-img">
-                    <form class="content">
-                        <div class="card-top">
-                            <h6>best c300 notes!</h6><p id="lecture">CS300</p>
-                        </div>
-                        <p>Lorem20Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias voluptate assumenda harum quam sapiente velit eaque veritatis incidunt officia consequuntur.</p>
-                        <label for="posted">Posted By: </label>
-                        <button id="posted-name" name="posted" style="background: #DCDCDC;border: none;">Jhon Doe</button>
-                        <br>
-                        <label for="other">Contact info: </label>
-                        <button id="mail" name="other" style="background: #DCDCDC;border: none;">example@email.com</button>
-                    </form>
-                    <div class="two-box">
-                    <svg xmlns="http://www.w3.org/2000/svg" style ="cursor: pointer;" width="24"       height="24" fill="red" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                    </svg>
-                    <h6>5$</h6>
+                        <button style="background: none;border: none">
+                        <section style="padding: 0;">
+                            <span class="trash" style="transform: scale(0.3);cursor: pointer;">
+                                <span></span>
+                                <i></i>
+                            </span>
+                        </section>
+                        </button>
+                        <h6>5$</h6>
                     </div>
                 </div>
             </section>
